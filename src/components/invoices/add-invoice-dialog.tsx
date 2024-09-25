90000b703f97e7f906c35009a9cc21b03bc3a4c4
Alfred Louis <alfredlouisw8@gmail.com>
2024-09-25 10:34:39 +0700

    add overflow auto to dialog

--- src/components/invoices/add-invoice-dialog.tsx
+++ src/components/invoices/add-invoice-dialog.tsx
@@ -1,34 +1,34 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/components/ui/dialog";
-import InvoiceForm from "./form";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import InvoiceForm from './form'
 
 type Props = {
-	customerId: string;
-};
+  customerId: string
+}
 
 export default function AddInvoiceDialog({ customerId }: Props) {
-	return (
-		<Dialog>
-			<DialogTrigger asChild>
-				<Button>Tambah</Button>
-			</DialogTrigger>
-			<DialogContent className="max-w-5xl">
-				<DialogHeader>
-					<DialogTitle>Tambah Invoice</DialogTitle>
-				</DialogHeader>
-				<InvoiceForm
-					type="create"
-					customerId={customerId}
-					successMessage="Invoice berhasil ditambahkan"
-				/>
-			</DialogContent>
-		</Dialog>
-	);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Tambah</Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-5xl max-h-[calc(100vh-160px)] overflow-auto">
+        <DialogHeader>
+          <DialogTitle>Tambah Invoice</DialogTitle>
+        </DialogHeader>
+        <InvoiceForm
+          type="create"
+          customerId={customerId}
+          successMessage="Invoice berhasil ditambahkan"
+        />
+      </DialogContent>
+    </Dialog>
+  )
 }
--- src/components/services/add-service-dialog.tsx
+++ src/components/services/add-service-dialog.tsx
@@ -1,34 +1,34 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/components/ui/dialog";
-import ServiceForm from "./form";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import ServiceForm from './form'
 
 type Props = {
-	customerId: string;
-};
+  customerId: string
+}
 
 export default function AddServiceDialog({ customerId }: Props) {
-	return (
-		<Dialog>
-			<DialogTrigger asChild>
-				<Button>Tambah</Button>
-			</DialogTrigger>
-			<DialogContent className="max-w-5xl">
-				<DialogHeader>
-					<DialogTitle>Tambah Jasa</DialogTitle>
-				</DialogHeader>
-				<ServiceForm
-					type="create"
-					customerId={customerId}
-					successMessage="Jasa berhasil ditambahkan"
-				/>
-			</DialogContent>
-		</Dialog>
-	);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Tambah</Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-5xl max-h-[calc(100vh-160px)] overflow-auto">
+        <DialogHeader>
+          <DialogTitle>Tambah Jasa</DialogTitle>
+        </DialogHeader>
+        <ServiceForm
+          type="create"
+          customerId={customerId}
+          successMessage="Jasa berhasil ditambahkan"
+        />
+      </DialogContent>
+    </Dialog>
+  )
 }
